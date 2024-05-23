132ef7f4bb6bbdd912d7be27d42d76b4fa61883d
Kyle <kylejrgurlad@gmail.com>
2024-05-23 05:36:07 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci=[]
+    if length > 0:
+        fibonacci.append(0)
+    if length >= 2:
+        fibonacci.append(1)
+        for i in range(2, length):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)
